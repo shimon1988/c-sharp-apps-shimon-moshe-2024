bc5d214123ffa580cca9447e52c421b9fd11181c
Shimon Moshe <shimon1988@gmail.com>
2024-06-14 11:40:46 +0300

    save changes

--- c-sharp-apps-shimon moshe 2024/Program.cs
+++ c-sharp-apps-shimon moshe 2024/Program.cs
@@ -7,11 +7,14 @@ using System.Threading.Tasks;
 
 namespace c_sharp_apps_shimon_moshe_2024
 {
+
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            ProcessManager.MainProcess();
+            //ProcessManager.MainProcess();
         }
+        
     }
 }
---
+++ c-sharp-apps-shimon moshe 2024/sport-app/Round.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_shimon_moshe_2024.sport_app
+{
+    public class Round
+    {
+    }
+}
